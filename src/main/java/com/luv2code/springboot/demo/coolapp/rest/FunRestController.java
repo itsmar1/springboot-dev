48889f2c9aa72fe085098a3f3213ea09d71f4976
Marouane Ahal <d144013152@gmail.com>
2022-07-21 20:14:22 +0100

    create a REST API controller

---
+++ src/main/java/com/luv2code/springboot/demo/coolapp/rest/FunRestController.java
@@ -0,0 +1,16 @@
+package com.luv2code.springboot.demo.coolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that return "Hello World"
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World! Time on server is " + LocalDateTime.now();
+    }
+}
