43b07a124a71de2c64f65a2d524cb86fc5598092
Marouane Ahal <d144013152@gmail.com>
2022-07-22 02:34:38 +0100

    edit pom.xml and add spring-boot-devtools

--- src/main/java/com/luv2code/springboot/demo/coolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/coolapp/rest/FunRestController.java
@@ -11,6 +11,18 @@ public class FunRestController {
     // expose "/" that return "Hello World"
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World! Time on server is " + LocalDateTime.now();
+        return "Hello World!!!!!!! Time on server is " + LocalDateTime.now();
+    }
+
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k";
+    }
+
+    // expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day!";
     }
 }
